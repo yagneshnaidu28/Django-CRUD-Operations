f1f304fa06d25c447b2523dfff6244c405aea5b6
yagneshnaidu28 <yagneshnaidu28@gmail.com>
2026-04-06 12:37:31 +0530

    adding models

--- crud_app/models.py
+++ crud_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ClientInfo(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.CharField(max_length=100,unique=True)
+    phone=models.CharField(max_length=10,unique=True)
+    status=models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
--- crud_app/views.py
+++ crud_app/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from .models import ClientInfo
 
 # Create your views here.
 def home(request):
-    return render(request,"index.html")
+    client_data=ClientInfo.objects.all()
+    context={
+        'client_data':client_data,
+    }
+    return render(request,"index.html",context)
+
+def add_client(request):
+    if request.method == 'POST':
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        status=request.POST.get('status')
+        client=ClientInfo.objects.create(
+            name=name,
+            email=email,
+            phone=phone,
+            status=status,
+        )
+        client.save()
+        return redirect('home')
+    return render(request,'index.html')
